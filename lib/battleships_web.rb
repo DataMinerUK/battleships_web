86c5620b9ba5a69cbcf5eb2b4477e76fbd6639c5
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-24 15:19:22 +0100

    Refactoring such that when all ships are placed the battle begins

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -21,28 +21,37 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/place_ships' do
-    @ships = ["cruiser", "submarine", "destroyer", "battleship", "aircraft carrier"]
+    $ships = ["cruiser", "submarine", "destroyer", "battleship", "aircraft carrier"]
     erb :place_ships
   end
 
   post '/place_ships' do
-    @ships = ["cruiser", "submarine", "destroyer", "battleship", "aircraft carrier"]
+    # $ships = ["cruiser", "submarine", "destroyer", "battleship", "aircraft carrier"]
     ships_hash = {"cruiser" => Ship.cruiser,
                   "submarine"=> Ship.submarine,
                   "destroyer"=> Ship.destroyer,
                   "battleship"=> Ship.battleship,
                   "aircraft carrier"=> Ship.aircraft_carrier}
     @ship_type = params[:ship_type]
-    @ships.delete(@ship_type)
+    $ships.delete(@ship_type)
     @coordinate = (params[:x_coord] + params[:y_coord].to_s).to_sym
     @orienation = params[:orientation].to_sym
     $game.player_1.place_ship ships_hash[@ship_type], @coordinate, @orienation
     @board = $game.own_board_view $game.player_1
+    if $ships.empty?
+      redirect '/battle'
+    end
     erb :place_ships
   end
 
   get '/battle' do
     @board = $game.own_board_view $game.player_1
+    $game.player_2.place_ship Ship.cruiser, :C4, :vertically
+    $game.player_2.place_ship Ship.submarine, :H2, :horizontally
+    $game.player_2.place_ship Ship.destroyer, :D9, :horizontally
+    $game.player_2.place_ship Ship.battleship, :J5, :vertically
+    $game.player_2.place_ship Ship.aircraft_carrier, :A1, :horizontally
+    @opponent_board = $game.opponent_board_view $game.player_1
     erb :battle
   end
 
--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -3,9 +3,42 @@ require 'spec_helper'
 feature 'in battle' do
 
   scenario 'after placing ships we begin' do
-    visit '/place_ships'
-    click_button('Battle!')
-    expect(page).to have_content "Let's start the battle"
+    visit '/start'
+    fill_in 'name', with: 'Kirsten'
+    click_button('Submit')
+    click_button('Place ships')
+
+    select 'cruiser', from: 'ship_type'
+    select 'A', from: 'x_coord'
+    select 1, from: 'y_coord'
+    select 'vertically', from: 'orientation'
+    click_button('Place')
+
+    select 'submarine', from: 'ship_type'
+    select 'B', from: 'x_coord'
+    select 1, from: 'y_coord'
+    select 'vertically', from: 'orientation'
+    click_button('Place')
+
+    select 'destroyer', from: 'ship_type'
+    select 'C', from: 'x_coord'
+    select 1, from: 'y_coord'
+    select 'vertically', from: 'orientation'
+    click_button('Place')
+
+    select 'battleship', from: 'ship_type'
+    select 'D', from: 'x_coord'
+    select 1, from: 'y_coord'
+    select 'vertically', from: 'orientation'
+    click_button('Place')
+
+    select 'aircraft carrier', from: 'ship_type'
+    select 'E', from: 'x_coord'
+    select 1, from: 'y_coord'
+    select 'vertically', from: 'orientation'
+    click_button('Place')
+
+    expect(page).to have_content "Fire at your opponent's board"
   end
 
 
