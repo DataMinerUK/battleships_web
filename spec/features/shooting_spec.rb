48a0e69660056889c327221e1186ab381f7020a3
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-24 18:46:41 +0100

    ensuring a shot cannot be fired at the same coordinate again and if so the opponent doesn't shoot

--- spec/features/shooting_spec.rb
+++ spec/features/shooting_spec.rb
@@ -12,8 +12,27 @@ feature 'shotting at board' do
    select 'J', from: 'x_coord'
    select 10, from: 'y_coord'
    click_button('Fire!')
-   save_and_open_page
+   # save_and_open_page
    expect(page).to have_content('Your opponent')
  end
+
+   skip 'you cannot shoot at the same coordinate more than once' do
+      visit '/start'
+      fill_in 'name', with: 'Kirsten'
+      click_button('Submit')
+
+      click_button('Place ships')
+
+      visit '/battle'
+      # select 'J', from: 'x_coord'
+      # select 10, from: 'y_coord'
+      click_button('Fire!')
+
+      # select 'J', from: 'x_coord'
+      # select 10, from: 'y_coord'
+      click_button('Fire!')
+
+      expect(page).to have_content("You already shot there")
+   end
 
 end
