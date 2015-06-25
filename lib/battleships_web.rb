af6d53fe8d93cac97f661329046cecece702af0e
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-25 22:21:12 +0100

    Attempting to create player sessions but all ships appear on each player's board, even opponents

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -7,15 +7,18 @@ class BattleshipsWeb < Sinatra::Base
 
   set :views, proc { File.join(root, '..', 'views') }
 
+  PLAYERS = ['player_2', 'player_1']
+
   get '/' do
     erb :index
   end
 
   get '/start' do
-    $visitor = params[:name]
-    if $visitor
-      $game = Game.new Player, Board
-      @board = $game.own_board_view $game.player_1
+    @visitor = params[:name]
+    if @visitor
+      $game ||= Game.new Player, Board
+      assign_player unless PLAYERS.empty?
+      @board = $game.own_board_view $game.send(session[:player])
       erb :welcome
     else
       erb :start
@@ -24,22 +27,23 @@ class BattleshipsWeb < Sinatra::Base
 
   get '/place_ships' do
     $ships = ["cruiser", "submarine", "destroyer", "battleship", "aircraft_carrier"]
+    @board = $game.own_board_view $game.send(session[:player])
     erb :place_ships
   end
 
   post '/place_ships' do
 
     ship_type = params[:ship_type]
-    coordinate = (params[:x_coord] + params[:y_coord])
+    coordinate = params[:x_coord] + params[:y_coord]
     orientation = params[:orientation]
 
     begin
-      $game.player_1.place_ship Ship.send(ship_type), coordinate, orientation
+      $game.send(session[:player]).place_ship Ship.send(ship_type), coordinate, orientation
       $ships.delete(ship_type)
     rescue RuntimeError => @error
     end
 
-    @board = $game.own_board_view $game.player_1
+    @board = $game.own_board_view $game.send(session[:player])
 
     if $ships.empty?
       redirect '/battle'
@@ -48,8 +52,8 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/battle' do
-    @board = $game.own_board_view $game.player_1
-    # @opponent_board = $game.opponent_board_view $game.player_1
+    @board = $game.own_board_view $game.send(session[:player])
+    @opponent_board = $game.opponent_board_view $game.send(session[:player])
 
     erb :battle
   end
@@ -60,15 +64,15 @@ class BattleshipsWeb < Sinatra::Base
 
     begin
 
-      result_of_your_shot = $game.player_1.shoot your_shot.to_sym
+      result_of_your_shot = $game.send(session[:player]).shoot your_shot.to_sym
       @to_tell = message_from_your_shot result_of_your_shot
 
     rescue RuntimeError => @do_not_shoot_two_times
 
     end
 
-    # @opponent_board = $game.opponent_board_view $game.player_1
-    @board = $game.own_board_view $game.player_1
+    @opponent_board = $game.opponent_board_view $game.send(session[:player])
+    @board = $game.own_board_view $game.send(session[:player])
 
     if $game.has_winner?
       redirect '/game_over'
@@ -78,30 +82,36 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/game_over' do
-    @message = $game.player_1.winner? ? "You won! :)" : "You lost :("
+    @message = $game.send(session[:player]).winner? ? "You won! :)" : "You lost :("
     erb :game_over
   end
 
-  def message_from_your_shot shot
-    if shot == :miss
-      "Your shot missed. Try again"
-    elsif shot == :hit
-      "You hit a ship!"
-    else
-      "You just sunk a ship!"
+  helpers do
+
+    def assign_player
+      session[:player] = PLAYERS.pop
+    end
+
+    def message_from_your_shot shot
+      if shot == :miss
+        "Your shot missed. Try again"
+      elsif shot == :hit
+        "You hit a ship!"
+      else
+        "You just sunk a ship!"
+      end
+    end
+
+    def message_from_opponent_shot shot
+      if shot == :miss
+        "Your opponent missed"
+      elsif shot == :hit
+        "Your opponent just hit one of your ships"
+      else
+        "Your opponent just sunk one of your ships!"
+      end
     end
   end
-
-  def message_from_opponent_shot shot
-    if shot == :miss
-      "Your opponent missed"
-    elsif shot == :hit
-      "Your opponent just hit one of your ships"
-    else
-      "Your opponent just sunk one of your ships!"
-    end
-  end
-
 
   run! if app_file == $0
 end
