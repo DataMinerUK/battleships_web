9f48b1029d5dd1c86dbf3dabd8f359372dad37eb
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-25 20:58:48 +0100

    Attempting to stub has_winner? method but still an error in test

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -3,7 +3,17 @@ require 'spec_helper'
 feature 'ending game' do
 
   skip 'if there is a winner the game announces it' do
-    # Not sure how to ensure a board has all ships sunk in a feature test
+    visit '/start'
+    fill_in 'name', with: 'Kirsten'
+    click_button('Submit')
+    click_button('Place ships')
+
+    visit '/battle'
+    select 'J', from: 'x_coord'
+    select 10, from: 'y_coord'
+    click_button('Fire!')
+
+    allow($game).to receive(:has_winner?).and_return(true)
     expect(page).to have_content "GAME OVER!"
 
   end
