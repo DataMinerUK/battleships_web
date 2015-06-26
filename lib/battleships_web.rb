0d21f1932b7ff808aaec9ef5fd1f5d2b604c79b7
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-26 10:37:10 +0100

    Refactoring to use method to get player from session

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -7,7 +7,7 @@ class BattleshipsWeb < Sinatra::Base
 
   set :views, proc { File.join(root, '..', 'views') }
 
-  PLAYERS = ['player_2', 'player_1']
+  PLAYERS = ['player_1', 'player_2']
 
   get '/' do
     erb :index
@@ -18,7 +18,7 @@ class BattleshipsWeb < Sinatra::Base
     if @visitor
       $game ||= Game.new Player, Board
       assign_player unless PLAYERS.empty?
-      @board = $game.own_board_view $game.send(session[:player])
+      @board = $game.own_board_view player_from_session
       erb :welcome
     else
       erb :start
@@ -27,7 +27,7 @@ class BattleshipsWeb < Sinatra::Base
 
   get '/place_ships' do
     $ships = ["cruiser", "submarine", "destroyer", "battleship", "aircraft_carrier"]
-    @board = $game.own_board_view $game.send(session[:player])
+    @board = $game.own_board_view player_from_session
     erb :place_ships
   end
 
@@ -38,12 +38,12 @@ class BattleshipsWeb < Sinatra::Base
     orientation = params[:orientation]
 
     begin
-      $game.send(session[:player]).place_ship Ship.send(ship_type), coordinate, orientation
+      player_from_session.place_ship Ship.send(ship_type), coordinate, orientation
       $ships.delete(ship_type)
     rescue RuntimeError => @error
     end
 
-    @board = $game.own_board_view $game.send(session[:player])
+    @board = $game.own_board_view player_from_session
 
     if $ships.empty?
       redirect '/battle'
@@ -52,8 +52,8 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/battle' do
-    @board = $game.own_board_view $game.send(session[:player])
-    @opponent_board = $game.opponent_board_view $game.send(session[:player])
+    @board = $game.own_board_view player_from_session
+    @opponent_board = $game.opponent_board_view player_from_session
 
     erb :battle
   end
@@ -64,15 +64,15 @@ class BattleshipsWeb < Sinatra::Base
 
     begin
 
-      result_of_your_shot = $game.send(session[:player]).shoot your_shot.to_sym
+      result_of_your_shot = player_from_session.shoot your_shot.to_sym
       @to_tell = message_from_your_shot result_of_your_shot
 
     rescue RuntimeError => @do_not_shoot_two_times
 
     end
 
-    @opponent_board = $game.opponent_board_view $game.send(session[:player])
-    @board = $game.own_board_view $game.send(session[:player])
+    @opponent_board = $game.opponent_board_view player_from_session
+    @board = $game.own_board_view player_from_session
 
     if $game.has_winner?
       redirect '/game_over'
@@ -82,7 +82,7 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/game_over' do
-    @message = $game.send(session[:player]).winner? ? "You won! :)" : "You lost :("
+    @message = player_from_session.winner? ? "You won! :)" : "You lost :("
     erb :game_over
   end
 
@@ -90,6 +90,10 @@ class BattleshipsWeb < Sinatra::Base
 
     def assign_player
       session[:player] = PLAYERS.pop
+    end
+
+    def player_from_session
+      session[:player] == 'player_1' ? $game.player_1 : $game.player_2
     end
 
     def message_from_your_shot shot
